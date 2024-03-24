492c7015e02114aee35856b32ce6f99d26f9930c
phamhongphuc <phamhongphuc.info@gmail.com>
2024-03-24 17:38:15 +0700

    feat: create-next-app: next, typescript, scss, tailwindcss, eslint

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.scss";
+
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
