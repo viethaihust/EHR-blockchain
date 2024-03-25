c0b484df1f955f0234df850e2bd7a13e4f3b5b63
viethaihust <vuviethaihust@gmail.com>
2024-03-25 15:03:24 +0700

    merge

--- src/app/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-[calc(100vh-4rem)] flex-col items-center justify-between p-24 w-full bg-[url('../../public/doctor.jpg')] bg-cover">
-      
-    </main>
-  );
-}
