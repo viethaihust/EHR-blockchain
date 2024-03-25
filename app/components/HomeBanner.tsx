16c2b11bc4df3a2cf157abda0803f7d1d242a81b
viethaihust <vuviethaihust@gmail.com>
2024-03-25 19:45:54 +0700

    banner

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,9 +1,22 @@
+import { Button } from "antd";
 import React from "react";
-
 export default function HomeBanner() {
   return (
-    <div className="h-[60vh] bg-[url('/family.jpg')] bg-center bg-cover flex justify-center">
-      <h1 className="mt-28 text-4xl text-white">CHĂM SÓC SỨC KHỎE CỦA BẠN</h1>
+    <div className="h-[60vh] bg-[url('/banner.jpg')] bg-center bg-cover flex flex-col justify-center pl-12">
+      <h1 className="-mt-6 text-4xl font-bold text-white leading-snug">
+        CHĂM SÓC SỨC <br /> KHỎE CỦA BẠN
+      </h1>
+      <span className="mt-6 text-2xl text-white">
+        Lưu trữ thông tin bệnh án điện tử, <br /> dễ dàng truy cập mọi lúc mọi
+        nơi
+      </span>
+      <Button
+        type="primary"
+        size="large"
+        className="w-fit mt-6 ml-12 !font-bold"
+      >
+        Tìm hiểu thêm
+      </Button>
     </div>
   );
 }
