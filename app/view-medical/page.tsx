e9db5f4f0ec918320705169d88613f48d5566d04
viethaihust <vuviethaihust@gmail.com>
2024-04-08 22:21:47 +0700

    deploy to vercel

---
+++ app/view-medical/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useAccount, useReadContract } from "wagmi";
+import HomeHeader from "../components/HomeHeader";
+import { patientListContract } from "@/smart-contracts/ExampleAbi";
+
+export default function ViewMedical() {
+  const { address } = useAccount();
+  const [patientData, setPatientData] = useState<any>(null);
+
+  if (!address) {
+    console.log("Error");
+    return;
+  }
+  const { data: patientDetailData, isSuccess } = useReadContract({
+    ...patientListContract,
+    functionName: "getPatientMedicalList",
+    args: [address],
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setPatientData(patientDetailData);
+    }
+  }, [isSuccess, patientDetailData]);
+
+  console.log(patientData);
+
+  return (
+    <div>
+      <HomeHeader></HomeHeader>
+      {patientData && (
+        <div className="ml-20 mt-10">
+          <div className="mt-6 overflow-hidden">
+            <table className="table-auto">
+              <thead>
+                <tr>
+                  <th className="text-left text-xl leading-relaxed">
+                    Medical Details
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Weight
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.weight.toString()}kg
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Height
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.height.toString()}cm
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Blood Group
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.bloodGroup}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Disease Name
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.diseaseName}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Disease Description
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.diseaseDescription}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-emerald-500 bg-emerald-200 px-4 py-2 font-medium text-emerald-600">
+                    Disease Started On
+                  </td>
+                  <td className="border border-emerald-500 px-4 py-2 font-medium text-emerald-600">
+                    {patientData?.diseaseStartedOn}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
